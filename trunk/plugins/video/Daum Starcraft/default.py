50b9d849d01c95cdccf7be8f9861921882b09b66
seungjong.lee <seungjong.lee@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-01-24 01:52:13 +0000

    Quick fix for renovated Starcraft league page. MBC Starleague is removed from list. No match name & date are displayed yet. Daum changed movie location meta format.

--- trunk/plugins/video/Daum Starcraft/default.py
+++ trunk/plugins/video/Daum Starcraft/default.py
@@ -4,9 +4,8 @@ import urllib,urllib2,re,xbmcplugin,xbmcgui
 #TV DASH - by You 2008.
 
 def CATEGORIES():
-        addDir("01 - 프로리그","http://tvpot.daum.net/game/sl/LeagueList.do?league=pro",3,'')
-        addDir("02 - 온게임넷 스타리그","http://tvpot.daum.net/game/sl/LeagueList.do?league=osl",3,'')
-        addDir("03 - MSL","http://tvpot.daum.net/game/sl/LeagueList.do?league=msl",3,'')
+        addDir("01 - 프로리그","http://tvpot.daum.net/game/sl/LeagueList.do?league=pro&type=list",3,'')
+        addDir("02 - 온게임넷 스타리그","http://tvpot.daum.net/game/sl/LeagueList.do?league=osl&type=list",3,'')
 
 def STAR_LEAGUE(main_url):
         req = urllib2.Request(main_url)
@@ -14,14 +13,18 @@ def STAR_LEAGUE(main_url):
         response = urllib2.urlopen(req)
         link=response.read()
         response.close()
-        match=re.compile('''<div class="title">\s+(.+?)\s+</div>\s+<div class="map">(.+?)</div>\s+<div class="date">(.+?)</div>\s+<div class="btn">\s+<a href="#" onclick="showClipOption\(event, {clipid:'(.+?)',instid:'(.+?)' }''').findall(link)
 
-        if len( match) > 0:
-                count=1
-                for title,map,date,clipid,instid in match:
-                        url = "http://tvpot.daum.net/clip/ClipView.do?clipid=%s" % (clipid)
-                        VIDEOLINKS("%02d - %s[%s] - %s"%(count, title, map, date),url,'')
-                        count += 1
+	# NOTE: the below is not working at all. too long for lazy match?
+	#match1=re.compile('''<div class="matchGroup">\s*<h5><strong>(\d{4}.\d{2}.\d{2})</strong>([^<]*)</h5>\s*<p>\s*([^/]+)</p>(.+?)</ol>\s*</div>\s*</div>''').findall(link)
+
+	match=re.compile('''<strong class="set">([^<]+)</strong>\s*<dl>\s*<dt[^/]+</dt>\s*<dd class="player">\s*<a class=[^>]+>([^<]+)</a>\s*<em[^/]+</em>\s*</dd>\s*</dl>\s*<span>VS</span>\s*<dl class="right">\s*<dt[^/]+</dt>\s*<dd class="player">\s*<a class=[^>]+>([^<]+)</a>\s*<em[^/]+</em>\s*</dd>\s*</dl>\s*<a href="/clip/ClipView.do\?clipid=(\d+)" class="playBtn on">''').findall(link)
+
+	count=1
+	for setname,player1,player2,clipid in match:
+		url = "http://tvpot.daum.net/clip/ClipView.do?clipid=%s" % (clipid)
+		VIDEOLINKS("%02d - %s[%s vs %s]"%(count, setname, player1, player2),url,'')
+		count += 1
+
 
 def INDEX(main_url):
         req = urllib2.Request(main_url)
@@ -44,13 +47,13 @@ def INDEX(main_url):
 
 
 def VIDEOLINKS(name,url,thumbnail):
-#print name,url
+	#print "in videolink",name,url
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         response = urllib2.urlopen(req)
         link=response.read()
         response.close()
-        match=re.compile('''daumEmbed_.+?\('.+?','(.+?)','.+?'\)''').findall(link)
+        match=re.compile('''daumEmbed_.+?\('.+?','(.+?)','.+?','.+?'\)''').findall(link)
         for vid in match:
                 flv = DaumGetFlvByVid(url,vid)
                 addLink(name,flv,thumbnail)
@@ -59,24 +62,26 @@ def VIDEOLINKS(name,url,thumbnail):
 #Courtesy of Voinage, Coolblaze.        
 
 def DaumGetFLV(referer, url):
+    print "daum loc=" + url
     req = urllib2.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
     req.add_header('Referer', referer)
     page = urllib2.urlopen(req);response=page.read();page.close()
-    query_match = re.compile('''<MovieLocation movieURL="(.+?)"''').findall(response)
+    query_match = re.compile('''<MovieLocation movieURL="(.+?)" />''').findall(response)
     if len(query_match) > 0:
         return query_match[0]
     return None
 
 def DaumGetFlvByVid(referer, vid):
+    print "daum vid=" + str(vid)
     req = urllib2.Request("http://flvs.daum.net/viewer/MovieLocation.do?vid="+vid)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
     req.add_header('Referer', referer)
     page = urllib2.urlopen(req);response=page.read();page.close()
-    query_match = re.compile('''<MovieLocation url="(.+?)"/>''').findall(response)
+    query_match = re.compile('''<MovieLocation regdate="\d+" url="(.+?)" storage=".+?"/>''').findall(response)
     if len(query_match) > 0:
         query_match[0] = re.sub('&amp;','&',query_match[0])
-        print query_match[0]
+        #print query_match[0]
         return DaumGetFLV(referer, query_match[0])
 
 
