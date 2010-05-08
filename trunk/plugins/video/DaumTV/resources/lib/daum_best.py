38d31713f99d291409f964421d7a4a4496430b70
seungjong.lee <seungjong.lee@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-05-08 08:14:52 +0000

    Merge all daum plugins to one

---
+++ trunk/plugins/video/DaumTV/resources/lib/daum_best.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+"""
+  Best video clip
+"""
+import urllib
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+import re
+
+class DaumBestClip:
+    video_list = []
+    nextpage = None
+    def DaumBestClip(self):
+        pass
+    def parse(self,url):
+        link = urllib.urlopen(url)
+        soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+        self.video_list = []
+        self.nextpage = None
+        #-- item list
+        strain = SoupStrainer( "div", { "id" : "thumbList" } )
+        items = soup.find(strain).findAll('dl')
+        for item in items:
+            ddimg = item.find('dd',{'class' : 'img'})
+            vid_url = ddimg.find('a')['href']
+            if vid_url[0] == '/':
+                vid_url = "http://tvpot.daum.net"+vid_url
+            imgpt = ddimg.find('img')
+            thumb = imgpt['src']
+            title = imgpt['title']
+            self.video_list.append( (title,vid_url,thumb) )
+        #-- page
+        strain = SoupStrainer( "li", { "class" : re.compile('^bar11c') } )
+        items = soup.findAll(strain)
+        found = False;
+        for item in items:
+            if (found):
+            	temp = item.find('a')
+            	url = temp['href']
+            	if url[0] == '?':
+                    url = "http://tvpot.daum.net/best/BestToday.do"+url
+                self.nextpage = (temp['title'], url)
+                break
+            if item['class'].startswith("bar11c  selected"):
+            	found = True;
+
+if __name__ == "__main__":
+    #import sys,os
+    #LIB_DIR = os.path.normpath(os.path.join(os.getcwd(), '..', '..'))
+    #if not LIB_DIR in sys.path:
+    #    sys.path.append (LIB_DIR)
+
+    site = DaumBestClip()
+    site.parse("http://tvpot.daum.net/best/BestToday.do?svctab=best&range=0")
+    print "--- BEST 0 -----------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
+    print site.nextpage
+
+    site = DaumBestClip()
+    site.parse("http://tvpot.daum.net/best/BestToday.do?svctab=best&range=1")
+    print "--- BEST 1 -----------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
+    print site.nextpage
+
+    site = DaumBestClip()
+    site.parse("http://tvpot.daum.net/best/BestToday.do?svctab=best&range=2")
+    print "--- BEST 2 -----------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
+    print site.nextpage
+
+    site = DaumBestClip()
+    site.parse("http://tvpot.daum.net/best/BestToday.do?svctab=10m")
+    print "--- 10m -------------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
+    print site.nextpage
---
+++ trunk/plugins/video/DaumTV/resources/lib/daum_trailer.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+"""
+  Movie Trailer
+"""
+import urllib
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+import re
+
+class DaumTrailer:
+    video_list = []
+    def DaumTrailer(self):
+        pass
+    def parse(self,url):
+        link = urllib.urlopen(url)
+        soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+        self.video_list = []
+        #-- item list
+        strain = SoupStrainer( "td", { "class" : re.compile("^td2") } )
+        items = soup.findAll(strain)
+        for item in items:
+            refs = item.findAll('a')
+            vid_url = refs[-1]['href']
+            title   = refs[-1].contents[0]
+            if len(refs) > 1:
+                thumb = refs[0].find('img')['src']
+            self.video_list.append( (title,vid_url,thumb) )
+
+if __name__ == "__main__":
+    #import sys,os
+    #LIB_DIR = os.path.normpath(os.path.join(os.getcwd(), '..', '..'))
+    #if not LIB_DIR in sys.path:
+    #    sys.path.append (LIB_DIR)
+
+    site = DaumTrailer()
+    site.parse("http://movie.daum.net/ranking/movieclip_ranking/bestTrailer.do?datekey=3")
+    print "--- Trailer 0 -----------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
+
+    site.parse("http://movie.daum.net/ranking/movieclip_ranking/bestTrailer.do?datekey=2")
+    print "--- Trailer 1 -----------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
+
+    site.parse("http://movie.daum.net/ranking/movieclip_ranking/bestTrailer.do?datekey=1")
+    print "--- Trailer 2 -----------------------------"
+    print site.video_list[0]
+    print len(site.video_list)
