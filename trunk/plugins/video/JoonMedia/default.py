0fcc09e6084c30bf103894f78f33d65f9a65b22f
seungjong.lee <seungjong.lee@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-05-08 16:03:24 +0000

    use html parser instead of regular expression becomes more stable

--- trunk/plugins/video/JoonMedia/default.py
+++ trunk/plugins/video/JoonMedia/default.py
@@ -3,248 +3,164 @@
   JoonMedia - Korea Drama/TV Shows Streaming Service
 """
 
-import urllib,urllib2,re,xbmcplugin,xbmcgui
+import urllib,xbmcplugin,xbmcgui
 
 # plugin constants
-__plugin__ = "JoonMedia"
-__author__ = "edge"
-__url__ = "http://xbmc-korea.com/"
+__plugin__  = "JoonMedia"
+__author__  = "edge"
+__url__     = "http://xbmc-korea.com/"
 __svn_url__ = "http://xbmc-korean.googlecode.com/svn/trunk/plugins/video/JoonMedia"
 __credits__ = "XBMC Korean User Group"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 
 xbmc.log( "[PLUGIN] '%s: version %s' initialized!" % ( __plugin__, __version__, ), xbmc.LOGNOTICE )
 
-browser_hdr = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ko-KR; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+import os
+LIB_DIR = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
+if not LIB_DIR in sys.path:
+    sys.path.append (LIB_DIR)
+
+from BeautifulSoup import BeautifulSoup, SoupStrainer, NavigableString
+from getflv import GetFLV
 
 #-----------------------------------------------------
 def CATEGORIES():
     ## not parsing homepage for faster speed
-    addDir("최근 업데이트","http://joonmedia.net",6,"")
-    addDir("드라마","http://joonmedia.net/videos/dramas",1,"")
-    addDir("오락","http://joonmedia.net/videos/shows",1,"")
-    addDir("음악","http://joonmedia.net/videos/music",1,"")
-    addDir("다시보기","http://joonmedia.net/videos/classics",1,"")
-    addDir("영화","http://joonmedia.net/videos/movies",1,"")
-    addDir("일본영화","http://joonmedia.net/videos/jpmovies",1,"")
-    addDir("중국영화","http://joonmedia.net/videos/chmovies",1,"")
-    addDir("서양영화","http://joonmedia.net/videos/enmovies",1,"")
-    addDir("다큐","http://joonmedia.net/videos/docu",1,"")
-    addDir("시사교양","http://joonmedia.net/videos/edu",1,"")
+    addDir(u"최근 업데이트","http://joonmedia.net",2,"")
+    addDir(u"드라마","http://joonmedia.net/videos/dramas",1,"")
+    addDir(u"오락","http://joonmedia.net/videos/shows",1,"")
+    addDir(u"음악","http://joonmedia.net/videos/music",1,"")
+    addDir(u"다시보기","http://joonmedia.net/videos/classics",1,"")
+    addDir(u"영화","http://joonmedia.net/videos/movies",1,"")
+    addDir(u"일본영화","http://joonmedia.net/videos/jpmovies",1,"")
+    addDir(u"중국영화","http://joonmedia.net/videos/chmovies",1,"")
+    addDir(u"서양영화","http://joonmedia.net/videos/enmovies",1,"")
+    addDir(u"다큐","http://joonmedia.net/videos/docu",1,"")
+    addDir(u"시사교양","http://joonmedia.net/videos/edu",1,"")
 
 def VIDEO(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-    
-    mgrp=re.split('''<div class="column">\s*<div class="box">''', link.decode("utf-8"))
-    xbmc.log( "#tvshows=%d"%len(mgrp), xbmc.LOGDEBUG )
-    for mgdata in mgrp[1:]:	# skip the first block
-	tit_match = re.search('''<a href="(.*?)" class="arrow">(.*?)</a>''', mgdata)
-	url = tit_match.group(1)
-	title = tit_match.group(2)
-	thmb_match = re.search('''<img src="(.*?)" width="[1-9]''', mgdata)
-	thumb = thmb_match.group(1)
+    link = urllib.urlopen(main_url)
+    soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+    strain = SoupStrainer( "div", { "class" : "column" } )
+    for item in soup.findAll(strain):
+	ref = item.find('a')
+	title = ref.string
+	url = ref['href']
+	thumb = item.find('img')['src']
 	try:
 	    xbmc.log( "TV program: %s" % title.encode("euc-kr"), xbmc.LOGDEBUG )
 	except:
 	    pass    # skip unwanted encoding error (ex: Japanese character)
-	addDir(title, url, 2, thumb)
+	addDir(title, url, 3, thumb)
 
 def RECENT(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-    
-    mgrp=re.split('''<h2 align="center">''', link.decode("utf-8"))
-    xbmc.log( "#top categories=%d"%len(mgrp), xbmc.LOGDEBUG )
-    for mgdata in mgrp[1:]:	# skip the first block
-	category = re.match('''(.*?)</h2>''', mgdata).group(1)
-	addDir("--------------------------- "+category+" ---------------------------", "http://joonmedia.net", 6, '')
-	match = re.compile('''<a href="(.*?)" class="arrow">(.*?)</a>''').findall(mgdata)
-	for url,title in match:
+    link = urllib.urlopen(main_url)
+    soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+    strain = SoupStrainer( "div", { "class" : "column" } )
+    for item in soup.findAll(strain):
+	category = item.find('h2').contents[0]
+	addDir("---------------- %s ----------------" % category, '', 6, '')
+	for ref in item.findAll('a'):
+	    if str(ref.contents[0]).startswith('<strong>'):
+		continue	# skip
+	    title = ref.string
+	    url = ref['href']
 	    try:
 		xbmc.log( "TV program: %s" % title.encode("euc-kr"), xbmc.LOGDEBUG )
 	    except:
 		pass    # skip unwanted encoding error (ex: Japanese character)
-	    addDir(title, url, 2, '')
+	    addDir(title, url, 3, '')
 
 def TVSHOW(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-    
-    mgrp=re.split("</li><li>", link.decode("utf-8"))
-    xbmc.log( "#episode=%d"%len(mgrp), xbmc.LOGDEBUG )
-    for mgdata in mgrp:
-	tit_match = re.search('''<b>([^<]*)<br></b>''', mgdata)
-	title = tit_match.group(1)
-	xbmc.log( "Episode: %s" % title.encode("euc-kr"), xbmc.LOGDEBUG )
-	match2=re.compile('''<a href="([^"]*)" target="_blank">\s?(|<br/>)([^<]+)''').findall(mgdata)
-	for url,dummy,sup in match2:
-	    sup = sup.strip()
-	    title2 = "%s (%s)" % (title,sup)
-	    xbmc.log( "Found page: %s" % title2.encode("euc-kr"), xbmc.LOGDEBUG )
-	    if sup.find(u"멀티로딩")==0:
-		addDir( title2.replace(u"멀티로딩",u"유큐"), url, 3, '' )
-	    elif sup.find(u"하이스피드")==0:
-		addDir( title2, url, 3, '' )
-	    elif sup.find(u"토두")==0 or sup.find(u"56com")==0:
+    link = urllib.urlopen(main_url)
+    soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+    episodes = soup("div", {"class" : "column"})[3].findAll('li')
+    for episode in episodes:
+	title = ''.join(episode.find('b').findAll(text=lambda text:isinstance(text, NavigableString)))
+	for ref in episode.findAll('a'):
+	    url = ref['href']
+	    suppl = ''.join(ref.findAll(text=lambda text:isinstance(text, NavigableString))).strip()
+	    title2 = "%s (%s)" % (title,suppl)
+	    try:
+		xbmc.log( "Found page: %s" % title2.encode("euc-kr"), xbmc.LOGDEBUG )
+	    except:
+		pass
+	    if suppl.find(u"멀티로딩")==0:
+		addDir( title2.replace(u"멀티로딩",u"유큐"), url, 5, '' )
+	    elif suppl.find(u"토두")==0 or suppl.find(u"56com")==0:
 		addDir( title2, url, 4, '' )
-	    elif sup.find(u"베오")==0:
+	    elif suppl.find(u"베오")==0:
 		addDir( title2+u" [preview]", url, 4, '' )
-	    elif sup.find(u"유튜브")==0 or sup.lower()==u"youtube":
+	    elif suppl.find(u"하이스피드")==0:
 		addDir( title2, url, 5, '' )
-	    elif sup.find(u"데일리모션")==0:
+	    elif suppl.find(u"유튜브")==0 or suppl.lower()==u"youtube":
+		addDir( title2, url, 6, '' )
+	    elif suppl.find(u"데일리모션")==0:
 		addDir( title2, url, 7, '' )
+	    else:
+		print "Unexpected: %s" % suppl.encode('euc-kr')
 
 def EPISODE(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-
+    link = urllib.urlopen(main_url)
+    soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+    extr = GetFLV()
     i=0
-    match=re.compile('''vcastr_file=(\S*) ''').findall(link)
-    if match:
-	for flv in match:
-	    if flv.find('|')>0:
-		xbmc.log( "multifile parsing", xbmc.LOGDEBUG )
-		flvs=flv.split('|')
-		for flv2 in flvs:
-		    i=i+1;addLink("Part %d" % i, flv2, "")
-	    else:
-		i=i+1;addLink("Part %d" % i, flv, "")
-    else:
-	match=re.compile('''<embed [^>]*src="(.*?)"''').findall(link)
-	for flv in match:
-	    i=i+1; GetFLV("Part %d"%i, flv)
+    for item in soup.findAll('embed'):
+	swf = item['src']
+	thumb = extr.img(swf)
+	xbmc.log( "Container[0]: %s" % swf, xbmc.LOGDEBUG )
+	for flv in extr.flv(swf):
+	    i=i+1;addLink("Part %d" % i, flv, thumb)
 
-def EPISODE_HACK(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-
-    match=re.compile('''<embed src=['"](.*?)['"] ''').findall(link)
-    i=0;
-    for cntnr in match:
-	xbmc.log( "Container = %s" % cntnr, xbmc.LOGDEBUG )
-	i=i+1; GetFLV("Part %d"%i, cntnr)
+def EPISODE_DIRECT(main_url):
+    # BeautifulSoup can not handle a statement not protected by quote
+    import re
+    tDoc = urllib.urlopen(main_url).read().decode('utf-8')
+    i=0
+    blks = re.compile('src=\S*vcastr_file=(\S*)').findall(tDoc)
+    print "TEST: %d"%len(blks)
+    for blk in blks:
+	for vid in blk.split('|'):
+	    i=i+1;addLink("Part %d" % i, vid, '')
 
 def EPISODE_YOUTUBE(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-
-    match=re.compile('''flashvars="file=(.*?)&amp;''').findall(link)
-    if match:
-	i=0;
-	for cntnr in match:
-	    xbmc.log( "Container = %s" % cntnr, xbmc.LOGDEBUG )
-	    i=i+1; GetFLV("Part %d"%i, cntnr)
-	return
-
-    match=re.compile('''<embed src="(.*?)[&"]''').findall(link)
-    if match:
-	i=0;
-	for cntnr in match:
-	    xbmc.log( "Container = %s" % cntnr, xbmc.LOGDEBUG )
-	    i=i+1; GetFLV("Part %d"%i, cntnr)
-	return
-
-def EPISODE_FLASH(main_url):
-    req = urllib2.Request(main_url)
-    req.add_header('User-Agent', browser_hdr)
-    response=urllib2.urlopen(req);link=response.read();response.close()
-
-    match=re.compile('''<param name="movie" value="(.*?)"/>''').findall(link)
-    i=0;
-    for cntnr in match:
-	xbmc.log( "Container = %s" % cntnr, xbmc.LOGDEBUG )
-	i=i+1; GetFLV("Part %d"%i, cntnr)
-
-def GetFLV(name, url):
-    if url.find('tudou')>0:
-	req = urllib2.Request("http://www.flvcd.com/parse.php?kw="+url)
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);link=response.read();response.close()
-	match=re.search('<a href\s*=\s*"(.+?)" target="_blank" ', link)
-	if match:
-	    flv=re.sub('&amp;','&',match.group(1))
-	    flv=re.sub('\?1','?8',flv)	#trick to enable on-the-fly streaming
-	    addLink(name, flv, "http://www.video-download-capture.com/wp-content/uploads/2010/01/tudou_logo.jpg")
-    elif url.find('56.com')>0:
-	if url.find('vid=')<0:
-	    #obtain redirected url
-	    req = urllib2.Request(url)
-	    response=urllib2.urlopen(req);url=response.geturl();response.close()
-	req = urllib2.Request("http://www.flvcd.com/parse.php?kw="+url)
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);link=response.read();response.close()
-	match=re.search('<a href\s*=\s*"(.+?)" target="_blank" ', link)
-	if match:
-	    #obtain redirected url
-	    req = urllib2.Request(match.group(1))
-	    response=urllib2.urlopen(req);re_url=response.geturl();response.close()
-	    addLink(name, re_url, "http://mallow.wakcdn.com/avatars/000/060/094/normal.png")
-    elif url.find('youku')>0:
-	req = urllib2.Request("http://www.flvcd.com/parse.php?kw="+url)
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);link=response.read();response.close()
-	match=re.compile('<a href\s*=\s*"(.+?)" target="_blank" ').findall(link)
-	i=0;
-	for url in match:
-	    i=i+1;addLink(name+" part-"+str(i),url,"http://static.youku.com/v1.0.0541/index/img/youkulogo.gif")
-    elif url.find('veoh')>0:
-	#match=re.search(r'http://www.veoh.com/videos/(.+)',url)
-	match=re.search('permalinkId=(\w+)&',url)
-	req = urllib2.Request('http://www.veoh.com/rest/videos/'+match.group(1)+'/details')
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);link=response.read();response.close()
-
-	#preview has 5min play time limitation
-	veoh=re.search('fullPreviewHashPath="(.+?)"',link).group(1)
-	thumb=re.search('fullHighResImagePath="(.+?)"',link).group(1)
-	if veoh.find("content.veoh.com")>0:
-	    #obtain redirected url
-	    req = urllib2.Request(veoh)
-	    response=urllib2.urlopen(req);re_url=response.geturl();response.close()
-	    addLink(name, re_url, thumb)
+    link = urllib.urlopen(main_url)
+    soup = BeautifulSoup( link.read(), fromEncoding="utf-8" )
+    extr = GetFLV()
+    i=0
+    for item in soup.findAll('embed'):
+	try:
+	    swf = item['flashvars']
+	except:
+	    swf = None
+	if swf:
+	    ptn2 = 'file='
+	    swf = swf[swf.find(ptn2)+len(ptn2):swf.find('&amp;')]
+	    thumb = extr.img(swf)
+	    xbmc.log( "Container[1]: %s" % swf, xbmc.LOGDEBUG )
+	    for flv in extr.flv(swf):
+		i=i+1;addLink("Part %d" % i, flv, thumb)
 	else:
-	    addLink(name, veoh, thumb)
-    elif url.find('youtube')>0:
-	id = re.search('http://www.youtube.com/watch\?v=(.+)',url)
-	if id is None:
-	    id = re.search('http://www.youtube.com/v/(.+)',url)
-	    url = "http://www.youtube.com/watch?v=%s"%id.group(1)
-	xbmc.log( "youtube ID: "+id.group(1), xbmc.LOGDEBUG )
-	req = urllib2.Request(url)
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);link=response.read();response.close()
-	key = re.search('&t=(.+?)&',link)
-	if key:
-	    addLink(name,"http://www.youtube.com/get_video.php?video_id="+id.group(1)+"&t="+key.group(1),"http://s.ytimg.com/yt/img/logos/youtube_logo_standard_againstwhite-vfl95119.png")
-	    addLink(name+" HQ","http://www.youtube.com/get_video.php?video_id="+id.group(1)+"&t="+key.group(1)+"&fmt=18","http://s.ytimg.com/yt/img/logos/youtube_logo_standard_againstblack-vfl95119.png")
-    elif url.find('4shared')>0:
-	req = urllib2.Request(url)
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);re_url=response.geturl();response.close()
-	match = re.search("streamer=(.*?)&",re_url)
-	if match:
-	    addLink(name, match.group(1), "http://userlogos.org/files/logos/veinedstorm/4shared.png")
-    elif url.find('dailymotion')>0:
-	id = re.search('http://www.dailymotion.com/.*?/(.*)',url).group(1)
-	xbmc.log( "dailymotion ID: "+id, xbmc.LOGDEBUG )
+	    swf = item['src']
+	    if '&' in swf:
+		swf = swf[:swf.find('&')]
+	    thumb = extr.img(swf)
+	    xbmc.log( "Container[2]: %s" % swf, xbmc.LOGDEBUG )
+	    for flv in extr.flv(swf):
+		i=i+1;addLink("Part %d" % i, flv, thumb)
 
-	req = urllib2.Request("http://www.dailymotion.com/video/"+id)
-	req.add_header('User-Agent', browser_hdr)
-	response=urllib2.urlopen(req);link=response.read();response.close()
-	match=re.search('''addVariable\("video", "(.*?)"\);''', link)
-	if match:
-	    xbmc.log( "dailymotion wrapper: "+match.group(1), xbmc.LOGDEBUG )
-	    #obtain redirected url
-	    req = urllib2.Request(match.group(1))
-	    response=urllib2.urlopen(req);re_url=response.geturl();response.close()
-	    addLink(name, re_url, "http://www.iconspedia.com/uploads/1687271053.png")
+def EPISODE_PARAM(main_url):
+    link = urllib.urlopen(main_url)
+    strain = SoupStrainer( "param", { "name" : "movie" } )
+    soup = BeautifulSoup( link.read(), strain, fromEncoding="utf-8" )
+    extr = GetFLV()
+    i=0
+    for item in soup.findAll('param'):
+	cntnr=item['value']
+	xbmc.log( "Container[3]: %s" % cntnr, xbmc.LOGDEBUG )
+	thumb = extr.img(cntnr)
+	for flv in extr.flv(cntnr):
+	    i=i+1;addLink("Part %d" % i, flv, thumb)
 
 #-----------------------------------                
 def get_params():
@@ -268,6 +184,7 @@ def get_params():
 
 def addLink(name,url,iconimage):
     ok=True
+    name=name.encode("utf-8")
     xbmc.log( "addLink(%s,%s)" % (name, url), xbmc.LOGDEBUG )
     liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
     liz.setInfo( type="Video", infoLabels={ "Title": name } )
@@ -275,6 +192,7 @@ def addLink(name,url,iconimage):
     return ok
 
 def addDir(name,url,mode,iconimage):
+    name=name.encode("utf-8")
     u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
     ok=True
     liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
@@ -311,16 +229,16 @@ if mode==None or url==None or len(url)<1:
 elif mode==1:
     VIDEO(url)
 elif mode==2:
-    TVSHOW(url)
-elif mode==3:
-    EPISODE(url)
-elif mode==4:
-    EPISODE_HACK(url)
-elif mode==5:
-    EPISODE_YOUTUBE(url)
-elif mode==6:
     RECENT(url)
+elif mode==3:
+    TVSHOW(url)
+elif mode==4:
+    EPISODE(url)
+elif mode==5:
+    EPISODE_DIRECT(url)
+elif mode==6:
+    EPISODE_YOUTUBE(url)
 elif mode==7:
-    EPISODE_FLASH(url)
+    EPISODE_PARAM(url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
