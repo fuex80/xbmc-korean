38e3af1146cef983d8159946b93ac4f28bf8947d
neurostar <neurostar@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-07-08 16:34:10 +0000

    cleanup addon.xml

---
+++ addons/metadata.artists.naver.com/build.bat
@@ -0,0 +1,30 @@
+@Echo off
+
+SET ScriptName=metadata.albums.naver.com
+:: Create Build folder
+Echo ------------------------------
+Echo Creating %ScriptName% Build Folder . . .
+rmdir BUILD /S /Q
+md BUILD
+Echo.
+
+:: Create exclude file
+Echo ------------------------------
+Echo Creating exclude.txt file . . .
+Echo.
+Echo .svn>"BUILD\exclude.txt"
+Echo Thumbs.db>>"BUILD\exclude.txt"
+Echo Desktop.ini>>"BUILD\exclude.txt"
+Echo BUILD.BAT>>"BUILD\exclude.txt"
+Echo.
+
+Echo ------------------------------
+Echo Copying required files to \Build\%ScriptName%\ folder . . .
+xcopy resources "BUILD\%ScriptName%\resources" /E /Q /I /Y /EXCLUDE:BUILD\exclude.txt
+xcopy *.* "BUILD\%ScriptName%\" /EXCLUDE:BUILD\exclude.txt
+Echo.
+
+Echo Build Complete - Scroll Up to check for errors.
+Echo Final build is located in the BUILD folder.
+Echo copy: \%ScriptName%\ folder in the \BUILD\ folder.
+pause
