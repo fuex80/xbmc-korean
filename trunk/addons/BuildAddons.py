d5fcf38e4a13b79860165bb164bcdc48c8b246b4
neurostar <neurostar@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-08-20 16:41:26 +0000

    fix md5 file for BuildAddons.py

--- trunk/addons/BuildAddons.py
+++ trunk/addons/BuildAddons.py
@@ -13,17 +13,22 @@ def main():
     generate_md5_file(addons_repo)
 
 def addon_packager(addons_repo):
+    if (not os.path.exists(addons_repo)):
+        print ("Please set Addon Repo path properly")
+        # fix:  need to stop script here
     addon_path = addon_selector();
-    add_version =addon_ver(addon_path)
     addon_id=os.path.basename(addon_path)
+    add_version =addon_ver(addon_path, addon_id)
     if (not os.path.exists(os.path.join(addons_repo, addon_id))):
         os.mkdir(os.path.join(addons_repo, addon_id))
+        print ("Directory for addon in Repo is not exist, Making new one...")
     for filename in glob.glob(os.path.join(addons_repo, addon_id, '*')) :
         os.remove( filename )
 
     zipper(addon_id, os.path.join(addons_repo, addon_id, addon_id + '-' + add_version + '.zip'))
     shutil.copyfile(os.path.join(addon_id, 'changelog.txt'), os.path.join(addons_repo, addon_id, 'changelog'+ '-' + add_version + '.txt'))
     shutil.copyfile(os.path.join(addon_id, 'icon.png'), os.path.join(addons_repo, addon_id, 'icon.png'))
+    print ("Packaged addon is placed in Addon repo\n")
 
 def addon_selector():
     addons_path=os.getcwd()
@@ -49,13 +54,19 @@ def addon_selector():
     else:
         addon_id=addon_lists[int(addon_selnum)]
         addon_path=os.path.join(addons_path, addon_id)
-    print "you select ", addon_id
+    print ("you select " + addon_id + "\n")
     return addon_path
 
-def addon_ver(addon_path):
+def addon_ver(addon_path, addon_id):
     tree = ElementTree.parse(addon_path + "\\addon.xml")
     add_root = tree.getroot()
     add_version = add_root.attrib['version']
+    add_id = add_root.attrib['id']
+    if (not add_id == addon_id):
+        print (" Addon ID is not matched with addon directory name, please check !")
+        # fix:  need to stop script here
+    else:
+        print ("Processing " + addon_id + " version " + add_version + "\n")
     return add_version
 
 def zipper(dir, zip_file):
@@ -72,6 +83,7 @@ def zipper(dir, zip_file):
     return zip_file
 
 def generate_addons_xml(addons_repo):
+    print ("Generating addons.xml from current addon files in repo\n")
     addons = os.listdir(addons_repo)
     addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>\n"
     for addon in addons:
@@ -93,7 +105,7 @@ def generate_addons_xml(addons_repo):
             # we succeeded so add to our final addons.xml text
             addons_xml += addon_xml.rstrip() + "\n\n"
         except:
-            print ("skipping", addon)
+            print ("Problems Found. skipping", addon)
     # clean and add closing tag
     addons_xml = addons_xml.strip() + u"\n</addons>\n"
     #print (addons_xml)
@@ -101,7 +113,7 @@ def generate_addons_xml(addons_repo):
 
 def generate_md5_file(addons_repo):
     # create a new md5 hash
-    m = md5.new( open( "addons.xml" ).read() ).hexdigest()
+    m = md5.new( open(os.path.join(addons_repo, "addons.xml") ).read() ).hexdigest()
     # save file
     open(os.path.join(addons_repo, 'addons.xml.md5'), "w" ).write(m)
 
