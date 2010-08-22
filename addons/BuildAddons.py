ab14452346d03b612c92a45fac4722ea654fb93f
neurostar <neurostar@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-08-22 02:35:53 +0000

    Prettier addons.xml by parsing xml in BuildAddons.py

--- addons/BuildAddons.py
+++ addons/BuildAddons.py
@@ -8,7 +8,7 @@ import Tkinter, tkFileDialog
 
 def main():
     addons_repo = (r"D:\xbmc-korean\xbmc-korea-addons\addons\dharma" )
-    addon_packager(addons_repo)
+    #addon_packager(addons_repo)
     generate_addons_xml(addons_repo)
     generate_md5_file(addons_repo)
 
@@ -85,37 +85,42 @@ def zipper(dir, zip_file):
 def generate_addons_xml(addons_repo):
     print ("Generating addons.xml from current addon files in repo\n")
     addons = os.listdir(addons_repo)
-    addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>\n"
+    addons_root=ElementTree.Element("addons")
     for addon in addons:
         try:
             # skip any file or .svn folder
             if ( not os.path.isdir( os.path.join(addons_repo, addon) ) or addon == ".svn" ): continue
-            # create path
             addon_zfilename = glob.glob(os.path.join( addons_repo, addon, '*.zip' ))
             addon_zfile = zipfile.ZipFile(addon_zfilename[0], "r" )
-            xml_lines = addon_zfile.open (addon + '/addon.xml').read().splitlines()
-            # new addon
-            addon_xml = ""
-            # loop thru cleaning each line
-            for line in xml_lines:
-                # skip encoding format line
-                if ( line.find( "<?xml" ) >= 0 ): continue
-                # add line
-                addon_xml += unicode( line.rstrip() + "\n", "utf-8" )
-            # we succeeded so add to our final addons.xml text
-            addons_xml += addon_xml.rstrip() + "\n\n"
+            zip_addon_xml = addon_zfile.open(addon + '/addon.xml')
+            addon_xml = ElementTree.parse(zip_addon_xml).getroot()
+            addons_root.append(addon_xml)
         except:
             print ("Problems Found. skipping", addon)
-    # clean and add closing tag
-    addons_xml = addons_xml.strip() + u"\n</addons>\n"
-    #print (addons_xml)
-    open(os.path.join(addons_repo,'addons.xml'), "w" ).write(addons_xml.encode( "utf-8" ))
+    indent(addons_root)
+    addons_xml=ElementTree.ElementTree(addons_root)
+    addons_xml.write(os.path.join(addons_repo,'addons.xml'), encoding="UTF-8")
 
 def generate_md5_file(addons_repo):
     # create a new md5 hash
     m = md5.new( open(os.path.join(addons_repo, "addons.xml") ).read() ).hexdigest()
     # save file
     open(os.path.join(addons_repo, 'addons.xml.md5'), "w" ).write(m)
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
 if __name__ == '__main__':
     main()
