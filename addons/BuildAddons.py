a0206cc1287b3c682c3344df1ba61cf7e1066b26
neurostar <neurostar@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-08-24 16:40:43 +0000

    BuildAddons - simplified menu

--- addons/BuildAddons.py
+++ addons/BuildAddons.py
@@ -1,4 +1,6 @@
 import os
+import re
+import sys
 import md5
 import glob
 import zipfile
@@ -7,25 +9,101 @@ from xml.etree import ElementTree
 import Tkinter, tkFileDialog
 
 addons_repo = (r"D:\xbmc-korean\xbmc-korea-addons\addons\dharma")
+addons_path = (r"")
 
 def main():
-    addon_packager(addons_repo)
+    addons_path = addons_path_check()
+    print ("\
+            ------------------------------------------------------\n \
+            1. Update or generate an addon from custom path\n \
+            2. Update all updated addons from current or specified path\n \
+            3. Just update addons.xml\n \
+           ------------------------------------------------------\n")
+    menu_selnum = raw_input("Please select number : ")
+    if menu_selnum == str(1):
+        addon_path = addon_selector()
+        addon_updater(addons_repo, addon_path)
+        addons_xml_updater(addons_repo)
+    elif menu_selnum == str(2):
+        addons_updater(addons_repo, addons_path)
+        addons_xml_updater(addons_repo)
+    elif menu_selnum == str(3):
+        addons_xml_updater(addons_repo)
+    else:
+        print ("Please select correct number !!")
+
+def addons_path_check():
+    if not os.path.exists(addons_repo):
+        sys.exit("Please set Addons Repo path properly")
+    try:
+        if os.path.exists(addons_path):
+            pass
+    except:
+        addons_path = os.getcwd()
+        return addons_path
+
+def addons_xml_updater(addons_repo):
     generate_addons_xml(addons_repo)
     generate_md5_file(addons_repo)
+    
+def addons_updater(addons_repo, addons_path):
+    addons = addon_match(addons_path)
+    for addon in addons:
+        addon_updater(addons_repo, os.path.join(addons_path, addon))
 
-def addon_packager(addons_repo):
-    if not os.path.exists(addons_repo):
-        print ("Please set Addons Repo path properly")
-        # fixme:  need to stop script here
-    addon_path = addon_selector();
+def addon_match(addons_path):
+    addons_candis = os.listdir(addons_path)
+    addon_pattern = re.compile("^((metadata|skin|repository|plugin|script|screensaver|visualization|weather)(\.[\w-]+)+)")
+    addon_ignore = ("repository")
+    addons = []
+    for addon in addons_candis:
+        if os.path.isdir(os.path.join(addons_path, addon)) and not addon_ignore in addon:
+            query = addon_pattern.search(addon)
+            if query:
+                matches = query.groups()
+                if matches[0] == addon:
+                    addons.append(addon)
+    return addons
+
+def addon_updater(addons_repo, addon_path):
+    addon_id = os.path.basename(addon_path)
+    addon_zip_num = len(glob.glob(os.path.join(addons_repo, addon_id, '*.zip')))
+    if os.path.isfile(os.path.join(addon_path, 'addon.xml')):
+        addon_version = addon_ver(addon_path)
+        if os.path.exists(os.path.join(addons_repo, addon_id)) and addon_zip_num == 1:
+            repo_addon_version = repo_add_ver(addons_repo, addon_id)
+            if addon_version == repo_addon_version:
+                print (addon_id + " is up to date.. version: " + addon_version)
+            elif addon_version > repo_addon_version:
+                print (addon_id + " is updated.. version: " + repo_addon_version + " to " + addon_version)
+                print ("Updating addon package for " + addon_id + " to version: " + addon_version)
+                addon_packer(addons_repo, addon_path)
+            elif addon_version < repo_addon_version:
+                print (addon_id + " is out dated.. version: " + addon_version)
+        elif not os.path.exists(os.path.join(addons_repo, addon_id)):
+            print ("\nFound new addon for " + addon_id)
+            os.mkdir(os.path.join(addons_repo, addon_id))
+            print ("Generating addon package for " + addon_id + " version: " + addon_version)
+            addon_packer(addons_repo, addon_path)
+        elif os.path.exists(os.path.join(addons_repo, addon_id)) and addon_zip_num == 0:
+            print ("\nThe addon path exist, But no old package found for " + addon_id)
+            print ("Copying addon package for " + addon_id + " version: " + addon_version)
+            addon_packer(addons_repo, addon_path)
+
+def repo_add_ver(addons_repo, addon):
+    addon_zfilename = glob.glob(os.path.join(addons_repo, addon, '*.zip'))
+    addon_zfile = zipfile.ZipFile(addon_zfilename[0], "r" )
+    zip_addon_xml = addon_zfile.open(addon + '/addon.xml')
+    addon_zfile.close()
+    repo_addon_xml = ElementTree.parse(zip_addon_xml).getroot()
+    repo_addon_version = repo_addon_xml.attrib['version']
+    return repo_addon_version
+
+def addon_packer(addons_repo, addon_path):
     addon_id = os.path.basename(addon_path)
     add_version = addon_ver(addon_path)
-    if not os.path.exists(os.path.join(addons_repo, addon_id)):
-        os.mkdir(os.path.join(addons_repo, addon_id))
-        print ("Directory for " + addon_id + " in the repository is not exist, Making new one...\n")
     for filename in glob.glob(os.path.join(addons_repo, addon_id, '*')) :
         os.remove(filename)
-
     addon_zipper(addon_id, os.path.join(addons_repo, addon_id, addon_id + '-' + add_version + '.zip'))
     try:
         shutil.copyfile(os.path.join(addon_id, 'changelog.txt'), os.path.join(addons_repo, addon_id, 'changelog'+ '-' + add_version + '.txt'))
@@ -38,33 +116,15 @@ def addon_packager(addons_repo):
     try:
         shutil.copyfile(os.path.join(addon_id, 'fanart.jpg'), os.path.join(addons_repo, addon_id, 'fanart.jpg'))
     except:
-        print ("Skipping icon.png for " + addon_id)
-    print ("Packaged " + addon_id + " is placed in Addon repo\n")
+        print ("Skipping fanart.jpg for " + addon_id)
+    print ("Packaged " + addon_id + " is placed in the repository\n")
+                       
 
 def addon_selector():
-    addons_path = os.getcwd()
-    addons = os.listdir(addons_path)
-    addon_num = 0
-    for addon in addons:
-        if os.path.isdir(addon) and addon != ".svn":
-            addon_num = addon_num +1
-    addon_lists = ['']*(addon_num+1)
-    print("0.Custom Addon Path")
-    addon_num = 0
-    for addon in addons:
-        if os.path.isdir(addon) and addon != ".svn":
-            addon_num = addon_num +1
-            print (str(addon_num) + '.' + addon)
-            addon_lists[addon_num] = addon
-    addon_selnum = raw_input("Choose an addon: ")
-    if int(addon_selnum) == 0:
-        root = Tkinter.Tk()
-        root.withdraw()
-        addon_path = tkFileDialog.askdirectory(parent=root,title='Please select a directory')
-        addon_id = os.path.basename(addon_path)
-    else:
-        addon_id = addon_lists[int(addon_selnum)]
-        addon_path = os.path.join(addons_path, addon_id)
+    root = Tkinter.Tk()
+    root.withdraw()
+    addon_path = tkFileDialog.askdirectory(parent=root,title='Please select a directory')
+    addon_id = os.path.basename(addon_path)
     print ("you select " + addon_id + "\n")
     return addon_path
 
@@ -73,16 +133,14 @@ def addon_ver(addon_path):
         addon_tree = ElementTree.parse(os.path.join(addon_path, 'addon.xml'))
     else:
         print("addon.xml is not exist")
-        # fixme:  need to stop script here
+        return
     addon_id = os.path.basename(addon_path)
     add_root = addon_tree.getroot()
     add_version = add_root.attrib['version']
     add_id = add_root.attrib['id']
     if not add_id == addon_id:
-        print (" Addon ID is not matched with addon directory name, please check !")
-        # fixme:  need to stop script here
-    else:
-        print ("Processing " + addon_id + " version " + add_version + "\n")
+        print ("Addon ID is not matched with addon directory name, please check !")
+        return
     return add_version
 
 def addon_zipper(dir, zip_file):
@@ -100,16 +158,14 @@ def addon_zipper(dir, zip_file):
     return zip_file
 
 def generate_addons_xml(addons_repo):
-    print ("Generating addons.xml from current addon files in repo\n")
-    addons = os.listdir(addons_repo)
+    addons = addon_match(addons_repo)
     addons_root = ElementTree.Element("addons")
     for addon in addons:
         try:
-            # skip any file or .svn folder
-            if (not os.path.isdir(os.path.join(addons_repo, addon)) or addon == ".svn"): continue
             addon_zfilename = glob.glob(os.path.join( addons_repo, addon, '*.zip' ))
             addon_zfile = zipfile.ZipFile(addon_zfilename[0], "r" )
             zip_addon_xml = addon_zfile.open(addon + '/addon.xml')
+            addon_zfile.close()
             addon_xml = ElementTree.parse(zip_addon_xml).getroot()
             addons_root.append(addon_xml)
         except:
@@ -117,7 +173,7 @@ def generate_addons_xml(addons_repo):
     indent(addons_root)
     addons_xml = ElementTree.ElementTree(addons_root)
     addons_xml.write(os.path.join(addons_repo,'addons.xml'), encoding="UTF-8")
-    print ("Sucessfuly generate addons.xml from current repository addons\n")
+    print ("\nSucessfuly generate addons.xml from current repository addons")
 
 def generate_md5_file(addons_repo):
     try:
@@ -125,7 +181,7 @@ def generate_md5_file(addons_repo):
         m = md5.new(open(os.path.join(addons_repo, "addons.xml")).read()).hexdigest()
         # save file
         open(os.path.join(addons_repo, 'addons.xml.md5'), "w" ).write(m)
-        print ("Sucessfuly generate addons.xml.md5 for addons.xml\n")
+        print ("Sucessfuly generate addons.xml.md5 for addons.xml")
     except:
         print ("An error occurred creating addons.xml.md5 file!\n")
 
