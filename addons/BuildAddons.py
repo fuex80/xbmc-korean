e057d8743a60a3332e51dba948afd3bb02f3446f
neurostar <neurostar@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2010-09-16 23:11:12 +0000

    add comments to BuildAddons.py

--- addons/BuildAddons.py
+++ addons/BuildAddons.py
@@ -8,16 +8,25 @@ import shutil
 from xml.etree import ElementTree
 import Tkinter, tkFileDialog
 
+#--------------------------------------------------------------------------------------------------
+# Please set the paths for repository and source addon
+# addons repo has to be set. either absolute or relative path
+# addons source path can be set or leave it if the scrpit is located in the addons source path
+#--------------------------------------------------------------------------------------------------
+
 addons_repo = (r"D:\xbmc-korean\xbmc-korea-addons\addons\dharma")
 addons_path = (r"")
 
+#--------------------------------------------------------------------------------------------------
+
+# Menu
 def main():
     addons_path = addons_path_check()
     print ("\
             ------------------------------------------------------\n \
             1. Update or generate an addon from custom path\n \
             2. Update all updated addons from current or specified path\n \
-            3. Just update addons.xml\n \
+            3. Just update addons.xml and md5 files for packages\n \
            ------------------------------------------------------\n")
     menu_selnum = raw_input("Please select number : ")
     if menu_selnum == str(1):
@@ -32,6 +41,7 @@ def main():
     else:
         print ("Please select correct number !!")
 
+# Check whether addon repo path exist
 def addons_path_check():
     if not os.path.exists(addons_repo):
         sys.exit("Please set Addons Repo path properly")
@@ -42,16 +52,19 @@ def addons_path_check():
         addons_path = os.getcwd()
         return addons_path
 
+# Generate addons.xml, md5 files for zip package, and md5 for addons.xml
 def addons_xml_updater(addons_repo):
     generate_addons_xml(addons_repo)
     md5_addon_package(addons_repo)
     generate_md5_file(addons_repo)
-    
+
+# Get addon list from the addon source path and update them all
 def addons_updater(addons_repo, addons_path):
     addons = addon_match(addons_path)
     for addon in addons:
         addon_updater(addons_repo, os.path.join(addons_path, addon))
 
+# Check whether directories are properly named addon directory
 def addon_match(addons_path):
     addons_candis = os.listdir(addons_path)
     addon_pattern = re.compile("^((metadata|skin|repository|plugin|script|screensaver|visualization|weather)(\.[\w-]+)+)")
@@ -66,6 +79,7 @@ def addon_match(addons_path):
                     addons.append(addon)
     return addons
 
+# Check the addon version in addon source and addon repo, then update repo package if addon source is newer
 def addon_updater(addons_repo, addon_path):
     addon_id = os.path.basename(addon_path)
     addon_zip_num = len(glob.glob(os.path.join(addons_repo, addon_id, '*.zip')))
@@ -91,6 +105,7 @@ def addon_updater(addons_repo, addon_path):
             print ("Copying addon package for " + addon_id + " version: " + addon_version)
             addon_packer(addons_repo, addon_path)
 
+# Get addon version number from repository package
 def repo_add_ver(addons_repo, addon):
     addon_zfilename = glob.glob(os.path.join(addons_repo, addon, '*.zip'))
     addon_zfile = zipfile.ZipFile(addon_zfilename[0], "r" )
@@ -100,6 +115,7 @@ def repo_add_ver(addons_repo, addon):
     repo_addon_version = repo_addon_xml.attrib['version']
     return repo_addon_version
 
+# Copy all addon files from addon source path to addon repo path
 def addon_packer(addons_repo, addon_path):
     addon_id = os.path.basename(addon_path)
     add_version = addon_ver(addon_path)
@@ -120,7 +136,7 @@ def addon_packer(addons_repo, addon_path):
         print ("Skipping fanart.jpg for " + addon_id)
     print ("Packaged " + addon_id + " is placed in the repository\n")
                        
-
+# Simle GUI for single addon selection
 def addon_selector():
     root = Tkinter.Tk()
     root.withdraw()
@@ -129,6 +145,7 @@ def addon_selector():
     print ("you select " + addon_id + "\n")
     return addon_path
 
+# Get addon version from addon source path
 def addon_ver(addon_path):
     if os.path.isfile(os.path.join(addon_path, 'addon.xml')):
         addon_tree = ElementTree.parse(os.path.join(addon_path, 'addon.xml'))
@@ -144,6 +161,7 @@ def addon_ver(addon_path):
         return
     return add_version
 
+# Zip addon package except .svn and Thumbs.db
 def addon_zipper(dir, zip_file):
     zip = zipfile.ZipFile(zip_file, 'w')
     root_len = len(os.path.abspath(dir))+1
@@ -158,6 +176,7 @@ def addon_zipper(dir, zip_file):
     zip.close()
     return zip_file
 
+# Generate addons.xml by parsing addon.xml in the zip packages
 def generate_addons_xml(addons_repo):
     addons = addon_match(addons_repo)
     addons_root = ElementTree.Element("addons")
@@ -176,6 +195,7 @@ def generate_addons_xml(addons_repo):
     addons_xml.write(os.path.join(addons_repo,'addons.xml'), encoding="UTF-8")
     print ("\nSucessfuly generate addons.xml from current repository addons")
 
+# Generate md5 file for addon zip package eq. addon.zip.md5 (only works if repository addon has <hashes>true</hashes>)
 def md5_addon_package(addons_repo):
     addons = addon_match(addons_repo)
     for addon in addons:
@@ -186,7 +206,8 @@ def md5_addon_package(addons_repo):
             open(addon_zfilename[0] + '.md5', "w" ).write(addon_md5)
         except:
             print ("Problems Found. skipping md5 file for ", addon)
-
+            
+# Generate md5 file for addons.xml
 def generate_md5_file(addons_repo):
     try:
         # create a new md5 hash
@@ -197,6 +218,7 @@ def generate_md5_file(addons_repo):
     except:
         print ("An error occurred creating addons.xml.md5 file!\n")
 
+# nice looking xml for human. took it from elementtree author's blog
 def indent(elem, level=0):
     i = "\n" + level*"  "
     if len(elem):
