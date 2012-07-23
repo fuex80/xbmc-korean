9d227d8c191765fbc0c29c0e39aadf1df53fc851
hojelei@gmail.com <hojelei@gmail.com@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2012-07-23 05:24:35 +0000

    cleanup baykorean follow up revived joonmedia.net(=>mjoon.com)

--- addons/eden/plugin.video.baykoreans.net/resources/lib/extract_tudou.py
+++ addons/eden/plugin.video.baykoreans.net/resources/lib/extract_tudou.py
@@ -2,11 +2,10 @@
 """
   Tudou
 """
-import urllib, re
+import urllib
 import xml.dom.minidom
-from random import randint
 
-def extract_video(iid):
+def revert_icode(iid):
   values = {
     'it' : iid,
     "hd" : "1",
@@ -17,15 +16,6 @@ def extract_video(iid):
   doc = hdl.read()
   hdl.close()
   dom = xml.dom.minidom.parseString(doc)
-  icode = dom.getElementsByTagName('v')[0].getAttribute('code')
-
-  url = "http://www.flvcd.com/parse.php?kw=http://www.tudou.com/programs/view/{0:s}/".format(icode)
-  hdl = urllib.urlopen(url)
-  doc = hdl.read().decode('gb2312')
-  hdl.close()
-  vid_url = re.search('<a href\s*=\s*"(http://\d.+?)" target="_blank"', doc).group(1)
-  vid_url = urllib.unquote(vid_url)
-
-  return vid_url
+  return dom.getElementsByTagName('v')[0].getAttribute('code')
 
 # vim:sts=2:sw=2:et
