868b04759876be4651442162f8d5cf56c7d206d8
hojelei@gmail.com <hojelei@gmail.com@f6c6f410-208d-11de-bf1e-7b5f7763e386>
2013-02-20 08:52:32 +0000

    script.xbmc-korea.lyrics   - mark as broken script.xbmc-korea.subtitles   - add cineast   - remove qple   - make subtitles module compatible to xbmc subtitles service module   - add smi2ass

---
+++ trunk/addons/frodo/script.xbmc-korea.subtitles/resources/lib/cineast_service.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*- 
+
+import sys
+import os
+from utilities import log, languageTranslate
+from cineast import CineastWebService
+
+_ = sys.modules[ "__main__" ].__language__   
+
+def search_subtitles( file_original_path, title, tvshow, year, season, episode, set_temp, rar, lang1, lang2, lang3, stack ): #standard input
+    msg = ""
+    subtitles_list = []
+    websvc = CineastWebService()
+    if len(tvshow) > 0:     # TvShow
+        for result in websvc.searchTvSubtitlesByTitle(tvshow):
+            subtitle = {"format": "smi", "sync": True}
+            subtitle['link'] = result['link']
+            subtitle['filename'] = result['title']
+            rating = 0
+            if season == result['season']:
+                rating += 3
+            if episode == result['episode']:
+                rating += 3
+            subtitle['rating'] = str(rating)
+            subtitle['language_name'] = result['language']
+            subtitle['language_flag'] = "flags/%s.gif" %languageTranslate(result['language'],0,2)
+            subtitles_list.append( subtitle )
+    else:   # Movie
+        title = title.strip()
+        if title[0]=='(' and title[-1]==')':
+            title = title[1:-1]
+            for result in websvc.searchMovieSubtitlesByTitle(title):
+                subtitle = {"format": "smi", "sync": True}
+                subtitle['link'] = result['link']
+                subtitle['filename'] = result['title']
+                subtitle['rating'] = "6" if file_original_path.find(result['relgrp']) > 0 else "0"
+                subtitle['language_name'] = result['language']
+                subtitle['language_flag'] = "flags/%s.gif" %result['language'][:2].lower()
+                subtitles_list.append( subtitle )
+    return subtitles_list, "", msg #standard output
+
+def download_subtitles (subtitles_list, pos, zip_subs, tmp_sub_dir, sub_folder, session_id): #standard input
+    language = subtitles_list[pos][ "language_name" ]
+    websvc = CineastWebService()
+    subtitle = websvc.parseSubtitlesPage( subtitles_list[pos]["link"] )[0]
+    log(__name__,  "download subtitle from %s" %subtitle["link"])
+    try:
+        tmp_fname = os.path.join(tmp_sub_dir, subtitle["filename"])
+        resp = urllib2.urlopen(url)
+        f = open(tmp_fname, "w")
+        f.write(resp.read())
+        f.close()
+    except:
+        return False, language, ""
+    return True, language, tmp_fname #standard output
--- trunk/addons/frodo/script.xbmc-korea.subtitles/resources/lib/gui.py
+++ trunk/addons/frodo/script.xbmc-korea.subtitles/resources/lib/gui.py
@@ -2,26 +2,41 @@
 """
   UI for subtitle download
 """
-import sys,os,xbmc,xbmcvfs
+import sys
+import os
+import xbmc, xbmcgui, xbmcvfs
 
 _ = sys.modules[ "__main__" ].__language__
 __scriptname__ = sys.modules[ "__main__" ].__scriptname__
 __settings__ = sys.modules[ "__main__" ].__settings__
 
-from gomtv_jmdb import *
-from qple_jamak import *
-from subt_down  import *
+import gomtv_service
+import cineast_service
+from smi2ass import smi2ass
 
 def gui():
+  if __settings__.getSetting( "smi2ass" )=='true':
+    smiPath = getExsitingSmi()
+    if smiPath is None:
+      smiPath = subt_down()
+    if smiPath is not None:
+      subt_conv(smiPath)
+      xbmc.executebuiltin("Notification('Subtitles','converted to ASS')")
+  else:
+    subt_down()
+
+def getExsitingSmi():
   movieFullPath = xbmc.Player().getPlayingFile()
   smiFullPath = movieFullPath[:movieFullPath.rfind('.')]+'.smi'
+  return smiFullPath if xbmcvfs.exists(smiFullPath) else None
 
-  try:
-    f=xbmcvfs.File(movieFullPath)
-  except IOError:
+def subt_down():
+  movieFullPath = xbmc.Player().getPlayingFile()
+  if not xbmcvfs.exists(movieFullPath):
     xbmc.log("can not open movie file, %s" % movieFullPath, xbmc.LOGERROR)
     xbmcgui.Dialog().ok("can not open movie file", movieFullPath)
-    sys.exit(1)
+    return None
+  smiFullPath = movieFullPath[:movieFullPath.rfind('.')]+'.smi'
 
   ###----- fetch list of available subtitles
   subt_list = []
@@ -29,20 +44,26 @@ def gui():
   ignored = dialog.create(__scriptname__ )
   if __settings__.getSetting( "GomTV" )=='true':
     dialog.update( 0, _(100)%_(200) )
-    found = gomtv_jamak_from_file(f)
-    if found is None:
+    subt_list1, temp, msg = gomtv_service.search_subtitles(movieFullPath, "", False, 0, 0, 0, False, False, "Korean", "English", "", False)
+    if len(subt_list1) == 0:
       xbmcgui.Dialog().ok(__scriptname__, _(101)%_(200), _(108) )
     else:
-      subt_list += found
-  if __settings__.getSetting( "Qple" )=='true':
-    dialog.update( 50, _(100)%_(201) )
-    found = qple_jamak_from_file(f)
-    if found is None:
-      xbmcgui.Dialog().ok(__scriptname__, _(101)%_(201), _(108) )
+      for i in range(len(subt_list1)):
+        subt_list1[i]["service"] = "gomtv"
+      subt_list += subt_list1
+  if __settings__.getSetting( "Cineast" )=='true':
+    dialog.update( 50, _(100)%_(202) )
+    infoTag = xbmc.Player().getVideoInfoTag()
+    title = infoTag.getTitle()
+    year = infoTag.getYear()
+    subt_list2, temp, msg = cineast_service.search_subtitles(movieFullPath, title.decode('utf-8'), "", year, 0, 0, False, False, "Korean", "English", "", False)
+    if len(subt_list2) == 0:
+      xbmcgui.Dialog().ok(__scriptname__, _(101)%_(202), _(108) )
     else:
-      subt_list += found
+      for i in range(len(subt_list2)):
+        subt_list2[i]["service"] = "cineast"
+      subt_list += subt_list2
   dialog.close()
-  f.close()
 
   ###----- select a subtitle to download
   if len(subt_list)==0:
@@ -52,30 +73,63 @@ def gui():
   else:
     title_list = []
     for i in range(0,len(subt_list)):
-      if subt_list[i][0] == 'gomtv':
-        title = "[Gom] "+subt_list[i][1]
-      elif subt_list[i][0] == 'qple':
-        title = "[Tok] "+subt_list[i][1]
+      if subt_list[i]["service"] == "gomtv":
+        title = "[Gom] "+subt_list[i]["filename"]
+      elif subt_list[i]["service"] == "cineast":
+        title = "[Cin] "+subt_list[i]["filename"]
       else:
-        title = subt_list[i][1]
+        title = subt_list[i]['filename']
       title_list.append( title )
-    print title_list
+    xbmc.log(str(title_list), xbmc.LOGDEBUG)
 
     dialog = xbmcgui.Dialog()
     selected = dialog.select( _(103)%len(title_list), title_list )
+    if selected < 0:
+      return None
 
-    if selected >= 0:
-      if subt_list[selected][0] == 'gomtv':
-        smiUrl = gomtv_jamak_url( subt_list[selected][2] )
-        if not smiUrl:
-          xbmcgui.Dialog().ok(__scriptname__, _(101)%_(200), _(108) )
-      else:
-        smiUrl = subt_list[selected][2]
+    smiTempDir = xbmc.translatePath( "special://temp/" )
+    smiTempPath = None
+    if subt_list[selected]["service"] == "gomtv":
+      succeed, lang, smiTempPath = gomtv_service.download_subtitles(subt_list, selected, False, smiTempDir, None, None)
+      if not succeed:
+        xbmcgui.Dialog().ok(__scriptname__, _(101)%_(200), _(108) )
+    elif subt_list[selected]["link"] == "cineast":
+      succeed, lang, smiTempPath = cineast_service.download_subtitles(subt_list, selected, False, smiTempDir, None, None)
+      if not succeed:
+        xbmcgui.Dialog().ok(__scriptname__, _(101)%_(202), _(108) )
+    if not smiTempPath:
+      return None
+    xbmc.log("subtitle is downloaded to "+smiTempPath, xbmc.LOGNOTICE)
 
-      if smiUrl:
-        smiPath = download_subtitle(smiUrl, smiFullPath)
-        xbmc.log("subtitle is downloaded to "+smiFullPath, xbmc.LOGNOTICE)
-        # enable the downloaded subtitle
-        if smiPath:
-          xbmc.Player().setSubtitles(smiPath)
+    # try to copy temp file to desired path
+    if xbmcvfs.copy(smiTempPath, smiFullPath):
+      smiPath = smiFullPath
+    else:
+      xbmc.log("Fail to write subtitles to "+smiFullPath, xbmc.LOGWARNING)
+      xbmcgui.Dialog().ok(__scriptname__, _(110), _(107), smiTempPath )
+      smiPath = smiTempPath
+    # enable the downloaded subtitle
+    xbmc.Player().setSubtitles(smiPath)
+    return smiPath
+
+def subt_conv(smiPath=None):
+  if smiPath is None:
+    videoDir = os.path.dirname(xbmc.Player().getPlayingFile())
+    smiPath = os.path.join(videoDir, xbmc.Player().getSubtitles())
+  if not xbmcvfs.exists(smiPath):
+    return
+  if not smiPath.lower().endswith(".smi"):
+    return
+  assDict = smi2ass(smiPath)
+  for lang in assDict:
+    ext = lang+'.ass'
+    if lang == '':
+      ext = 'ass'
+    assPath = smiPath[:smiPath.rfind('.')]+'.'+ext
+    if not xbmcvfs.exists(assPath) or __settings__.getSetting( "overwrite_ass" )=='true':
+      assfile = xbmcvfs.File(assPath, "w")
+      assfile.write(assDict[lang])
+      assfile.close()
+      xbmc.Player().setSubtitles(assPath)
+
 # vim: softtabstop=2 shiftwidth=2 expandtab
